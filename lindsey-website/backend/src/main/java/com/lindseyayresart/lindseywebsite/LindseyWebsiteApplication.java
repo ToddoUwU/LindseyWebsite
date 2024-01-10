c0b223d59ae65b79f4f6c76411ff5d54e718d3f7
Todd <ArmsOfUndertow98@gmail.com>
2024-01-09 19:03:10 -0600

    Tailwind and basic pom
    Added tailwind and the basic pom to the project

---
+++ lindsey-website/backend/src/main/java/com/lindseyayresart/lindseywebsite/LindseyWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.lindseyayresart.lindseywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindseyWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindseyWebsiteApplication.class, args);
+	}
+
+}
---
+++ lindsey-website/backend/src/test/java/com/lindseyayresart/lindseywebsite/LindseyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindseyayresart.lindseywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindseyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
